177eecca19b7a3cb898d52048db11e350041c992
Cara Heacock <caraheacock@gmail.com>
2014-04-07 13:44:24 -0500

    Created Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Butterflies::Application.config.secret_token = '80e482b8c9e6027b19cc6c303920b88b8ec74b977fda500361296406d929e01189a1a9e30a7ba52e71c13de00ac9eebb712223e520d302f43fa7d02920737e9a'
