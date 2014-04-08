96114260818b0ccb295924a365e3e2fe2f83808d
Cara Heacock <caraheacock@gmail.com>
2014-04-08 15:00:04 -0500

    More forms happened

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def authorize
+    if current_user.nil?
+      redirect_to :new_login
+    end
+  end
+  
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+  helper_method :current_user
 end
---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,27 @@
+class LoginsController < ApplicationController
+  def new
+    if current_user
+      redirect_to(:new_game)
+    else
+      @user = User.new
+    end
+  end
+  
+  def create
+    @user = User.find_by_username(params[:username])
+    
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      session[:username] = @user.username
+      redirect_to :new_game
+    else
+      @failed_login = true;
+      render "new"
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to(:root)
+  end
+end
