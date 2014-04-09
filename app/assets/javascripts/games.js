37f3d7494a57bbad51d9a7f407865ad86c53f9b5
Cara Heacock <caraheacock@gmail.com>
2014-04-09 16:56:50 -0500

    Game plays a single level

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -4,12 +4,17 @@ $("document").ready(function(){
   var totalSeconds;
   var timer;
   var gameIsOver = true;
+  var levelIsOver = true;
   
+  // Main Flow
   $("#start_button").click(function () {
     gameIsOver = false;
+    levelIsOver = false;
+    
     $("#start_button").fadeOut(3000, function() {
       $("#game_nav").append('<div id="game_timer"><p></p></div>');
       createTimer(30);
+      assignSections();
     });
     $("#start_button").text("Ready");
     setTimeout(setText, 1000);
@@ -56,7 +61,6 @@ $("document").ready(function(){
   }
   
   // Color Changing Mechanics
-  
   $("#sections polygon").click(changeColor);
   
   // function changeColor () {
@@ -80,7 +84,7 @@ $("document").ready(function(){
   // };
   
   function changeColor () {
-    if (!gameIsOver) {
+    if (!gameIsOver && !levelIsOver) {
       if ($(this).attr("class") == "red") {
         $(this).attr("class", "yellow");
       } else if ($(this).attr("class") == "yellow") {
@@ -92,6 +96,78 @@ $("document").ready(function(){
       } else {
         $(this).attr("class", "red");
       }
+      if (levelWon()) {
+        levelIsOver = true;
+        clearInterval(timer);
+        $("#game_timer p").text("Great job!");
+      }
     }
   }
+  
+  // Level Patterns
+  var pattern0 =
+  ["red", "red", "red", "yellow", "yellow", 
+  "yellow", "yellow", "yellow", "red", "red", 
+  "green", "green", "green", "blue", "blue", 
+  "blue", "blue", "blue", "green", "green"];
+  
+  var pattern1 =
+  ["blue", "blue", "blue", "blue", "blue", 
+  "blue", "green", "green", "green", "green", 
+  "red", "red", "red", "red", "red", 
+  "red", "yellow", "yellow", "yellow", "yellow"];
+  
+  var pattern2 =
+  ["red", "yellow", "yellow", "yellow", "yellow", 
+  "red", "red", "blue", "blue", "red", 
+  "red", "green", "green", "green", "green", 
+  "red", "red", "blue", "blue", "red"];
+  
+  var pattern3 =
+  ["green", "yellow", "red", "red", "yellow", 
+  "green", "blue", "red", "yellow", "blue", 
+  "green", "yellow", "red", "red", "yellow", 
+  "green", "blue", "red", "yellow", "blue"];
+  
+  var pattern4 =
+  ["red", "red", "red", "yellow", "yellow", 
+  "yellow", "green", "green", "blue", "blue", 
+  "blue", "blue", "blue", "green", "green",
+  "green", "yellow", "yellow", "red", "red"]
+  
+  var patterns = [pattern0, pattern1, pattern2, pattern3, pattern4];
+  
+  function assignSections () {
+    var chosenPattern = patterns[Math.floor(Math.random()*patterns.length)];
+    for (var i = 0; i < 20; i++) {
+      $("#sect_" + i).attr("class", chosenPattern[i]);
+    };
+  }
+  
+  // Win Logic
+  var nonLinearPairs = [[0, 5], [1, 4], [6, 9], [10, 15], [11, 14], [16, 19]];
+  
+  function linearMatch () {
+    for (var i = 0; i < 19; i++) {
+      if ( ($("#sect_" + i).attr("class") == $("#sect_" + (i+1)).attr("class")) && (i != 9) ) {
+        return true;
+      }
+    };
+    return false;
+  }
+  
+  function nonLinearMatch () {
+    for (var i = 0; i < nonLinearMatch.length; i++) {
+      if ($("#sect_" + nonLinearPairs[i][0]).attr("class") == $("#sect_" + nonLinearPairs[i][1])) {
+        return true;
+      }
+    };
+    return false;
+  }
+  
+  function levelWon () {
+    if (!linearMatch() && !nonLinearMatch()) {
+      return true;
+    }
+  }  
 });
