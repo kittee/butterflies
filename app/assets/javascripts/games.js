2368bc5be15f2057b26e86d7be14bce93a753716
Cara Heacock <caraheacock@gmail.com>
2014-04-08 20:37:38 -0500

    Started game mechanics and timer

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,2 +1,31 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$("document").ready(function(){
+  $("#start_button").on("click", function () {
+    $("#start_button").fadeOut(200, function() {
+      
+    });
+  });
+
+  var timer;
+  var totalSeconds;
+
+
+  function createTimer (timerID, time) {
+    timer = document.getElementById(timerID);
+    totalSeconds = time;
+
+    updateTimer()
+    window.setTimeout("Tick()", 1000);
+  }
+
+  function tick() {
+    totalSeconds -= 1;
+    updateTimer()
+    window.setTimeout("Tick()", 1000);
+  }
+
+  function updateTimer() {
+    timer.innerHTML = totalSeconds;
+  }
+});
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,8 @@ class GamesController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def index
+    @user = User.new
+  end
 end
