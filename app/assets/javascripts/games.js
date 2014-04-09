fb4fd3926d56ac12ad29ef7dcfc10c2c41854e73
Cara Heacock <caraheacock@gmail.com>
2014-04-08 21:56:44 -0500

    Timer working

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,31 +1,35 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $("document").ready(function(){
+  var totalSeconds;
+  var timer;
+  
   $("#start_button").on("click", function () {
     $("#start_button").fadeOut(200, function() {
-      
+      $("#game_nav").append('<div id="game_timer"><p></p></div>');
+      createTimer(30);
     });
   });
 
-  var timer;
-  var totalSeconds;
-
-
-  function createTimer (timerID, time) {
-    timer = document.getElementById(timerID);
+  function tick() {
+    if (totalSeconds <= 0) {
+      clearInterval(timer);
+      alert("Time's up!");
+      return;
+    }
+    
+    totalSeconds -= 1;
+    updateTimer();
+  }
+  
+  function createTimer (time) {
     totalSeconds = time;
 
-    updateTimer()
-    window.setTimeout("Tick()", 1000);
-  }
-
-  function tick() {
-    totalSeconds -= 1;
-    updateTimer()
-    window.setTimeout("Tick()", 1000);
+    updateTimer();
+    timer = setInterval(tick, 1000);
   }
 
   function updateTimer() {
-    timer.innerHTML = totalSeconds;
+    $("#game_timer p").text(totalSeconds);
   }
 });
