d216c49f162c71dcaf42ec0ab80632c319fbf868
Cara Heacock <caraheacock@gmail.com>
2014-04-09 15:41:40 -0500

    Oh god it's finally changing colors.

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -3,21 +3,36 @@
 $("document").ready(function(){
   var totalSeconds;
   var timer;
+  var gameIsOver = true;
   
-  $("#start_button").on("click", function () {
-    $("#start_button").fadeOut(200, function() {
+  $("#start_button").click(function () {
+    gameIsOver = false;
+    $("#start_button").fadeOut(3000, function() {
       $("#game_nav").append('<div id="game_timer"><p></p></div>');
       createTimer(30);
     });
+    $("#start_button").text("Ready");
+    setTimeout(setText, 1000);
+    setTimeout(goText, 2000);
+    
+    function setText () {
+      $("#start_button").text("Set");
+    }
+    
+    function goText () {
+      $("#start_button").text("Go!");
+    }
   });
-
+  
+  // Timer Mechanics
   function tick() {
     totalSeconds -= 1;
     updateTimer();
     
     if (totalSeconds <= 0) {
       clearInterval(timer);
-      alert("Time's up!");
+      $("#game_timer p").text("Game Over");
+      gameIsOver = true;
       return;
     }
   }
@@ -38,5 +53,45 @@ $("document").ready(function(){
       }
     };
     $("#game_timer p").text(totalSeconds + seconds_text());
+  }
+  
+  // Color Changing Mechanics
+  
+  $("#sections polygon").click(changeColor);
+  
+  // function changeColor () {
+  //   console.log("Colors!");
+  //   if ($(this).hasClass("red")) {
+  //     console.log(this);
+  //     console.log("It's red");
+  //     $(this).switchClass("red", "yellow", 200);
+  //   } else if ($(this).hasClass("yellow")) {
+  //     $(this).switchClass("yellow", "green", 200);
+  //   } else if ($(this).hasClass("green")) {
+  //     $(this).switchClass("green", "blue", 200);
+  //   } else if ($(this).hasClass("blue")) {
+  //     $(this).switchClass("blue", "red", 200);
+  //   } else {
+  //     console.log("No color");
+  //     console.log(this);
+  //     $("body").toggleClass("test");
+  //     $("#sect_0").toggleClass("red");
+  //   }
+  // };
+  
+  function changeColor () {
+    if (!gameIsOver) {
+      if ($(this).attr("class") == "red") {
+        $(this).attr("class", "yellow");
+      } else if ($(this).attr("class") == "yellow") {
+        $(this).attr("class", "green");
+      } else if ($(this).attr("class") == "green") {
+        $(this).attr("class", "blue");
+      } else if ($(this).attr("class") == "blue") {
+        $(this).attr("class", "red");
+      } else {
+        $(this).attr("class", "red");
+      }
+    }
   }
 });
