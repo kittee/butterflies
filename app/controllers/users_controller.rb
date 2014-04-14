7dd83cc2b67fe4eb1f99a1df1432560b121f6fba
Cara Heacock <caraheacock@gmail.com>
2014-04-14 17:01:10 -0500

    Optimized personal page queries to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,16 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by_username(params[:username])
-    @personal_top_games = @user.games.order('final_score').reverse[0..9]
+    #@personal_top_games = @user.games.order('final_score').reverse[0..9]
+    
+    # @personal_top_games = Game.joins(:user).select("games.*, users.username AS user_username").where("games.user_id" => @user.id).order('final_score').reverse[0..9]
+    # 
+    # @levels = Level.joins(@personal_top_games).select("levels.*")
+    
+    #@personal_top_games = User.includes(games: [:levels]).find_by_username(params[:username]).games.order('final_score').reverse[0..9]
+    
+    #@personal_top_games = Game.joins(:user, :levels).select("games.*, levels.level_num AS level_num").where("games.user_id" => @user.id).order('final_score').reverse[0..9]
+    
+    @personal_top_games = Game.includes(:levels).where("games.user_id" => @user.id).order('final_score').reverse[0..9]
   end
 end
